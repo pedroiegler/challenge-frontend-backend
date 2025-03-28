2ef87fd56d9d208b73ab749ec8918b80f7e18b9a
Pedro Iegler <produto@conor.com.br>
2025-03-28 16:48:31 -0300

    feat: adicionando script para popular um BD inicial com dados fictícios

---
+++ backend/populate_db.py
@@ -0,0 +1,66 @@
+#python manage.py shell < populate_db.py
+
+import django
+import os
+from decimal import Decimal
+from django.contrib.auth.models import User, Group
+from wallet.models import Wallet, Transaction
+from django.utils.timezone import now
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'financial_project.settings')
+django.setup()
+
+admin_group, created = Group.objects.get_or_create(name='admin')
+cliente_group, created = Group.objects.get_or_create(name='cliente')
+
+admin_user, created = User.objects.get_or_create(
+    username='admin_demo',
+    defaults={'email': 'admin@example.com', 'is_staff': True, 'is_superuser': True}
+)
+admin_user.set_password('admin123')
+admin_user.groups.add(admin_group)
+admin_user.save()
+
+
+client1, created = User.objects.get_or_create(
+    username='cliente1',
+    defaults={'email': 'cliente1@example.com', 'is_staff': False, 'is_superuser': False}
+)
+client1.set_password('cliente123')
+client1.groups.add(cliente_group)
+client1.save()
+
+
+client2, created = User.objects.get_or_create(
+    username='cliente2',
+    defaults={'email': 'cliente2@example.com', 'is_staff': False, 'is_superuser': False}
+)
+client2.set_password('cliente123')
+client2.groups.add(cliente_group)
+client2.save()
+
+
+wallet_admin, created = Wallet.objects.get_or_create(user=admin_user)
+if created or wallet_admin.balance == Decimal('0.00'):
+    wallet_admin.balance = Decimal('1000.00')
+    wallet_admin.save()
+
+wallet_client1, created = Wallet.objects.get_or_create(user=client1)
+if created or wallet_client1.balance == Decimal('0.00'):
+    wallet_client1.balance = Decimal('500.00')
+    wallet_client1.save()
+
+wallet_client2, created = Wallet.objects.get_or_create(user=client2)
+if created or wallet_client2.balance == Decimal('0.00'):
+    wallet_client2.balance = Decimal('200.00')
+    wallet_client2.save()
+    
+
+Transaction.objects.create(
+    sender=wallet_client1,
+    receiver=wallet_client2,
+    amount=Decimal('50.00'),
+    created_at=now()
+)
+
+print("Database successfully populated!")
