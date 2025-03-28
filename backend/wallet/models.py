daa35df192c5b433489752ffb31f5a3c21cc8ba6
Pedro Iegler <produto@conor.com.br>
2025-03-28 10:21:30 -0300

    feat: aplicando migrações do banco de dados

--- backend/wallet/models.py
+++ backend/wallet/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+class Transaction(models.Model):
+    sender = models.ForeignKey(Wallet, related_name='sent_transactions', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(Wallet, related_name='received_transactions', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
